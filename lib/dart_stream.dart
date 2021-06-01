2c2d92f2e1c04136d60cf95adce3fc3fd8ed4165
vendaMac <cavenda@outlook.com>
2021-06-01 14:02:26 +0800

    add example

--- lib/dart_stream.dart
+++ lib/dart_stream.dart
@@ -22,30 +22,3 @@ part 'src/opDistinctOps.dart';
 part 'src/opSortedOps.dart';
 part 'src/collectors.dart';
 
-
-
-void main(){
-  var head = _Head<int,int>.source(_ArrayIterator<int>([1,2,3]), 0);
-  var findFirst = head.map((t) => t+1).findFirst();
-  print(findFirst);
-
-  print(DartStream.of([1,2,3]).map((t) => t-1).allMatch((t) => t>0));
-  print(DartStream.of([1,2,3]).map((t) => t-1).sum());
-  print(DartStream.of([1.5,2.5,3.5]).map((t) => t-1).sum());
-  print(DartStream.of([1,2,3]).map((t) => t-1).average());
-
-  print(DartStream.of([1,2,3]).map((t) => t-1).flatMap((t) => DartStream.of([t,t,t]))
-      .limit(6).toList());
-
-  print(DartStream.of([1,2,3]).map((t) => t-1).flatMap((t) => DartStream.of([t,t,t]))
-      .skip(6).toList());
-
-  print(DartStream.of([1,2,3]).map((t) => t-1).flatMap((t) => DartStream.of([t,t,t]))
-      .filter((t) => t>0).skip(6).toList());
-
-  print(DartStream.of([1,2,3]).map((t) => t-1).flatMap((t) => DartStream.of([t,t,t]))
-      .sorted((o1,o2)=>o2-o1).toList());
-
-  print(DartStream.of([3,2,1]).map((t) => t-1).sorted().toList());
-  print(DartStream.of([3,2,1]).map((t) => t-1).sorted().map((t) => t.toString()+"!").toList());
-}
